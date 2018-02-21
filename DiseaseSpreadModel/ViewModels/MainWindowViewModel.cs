b5534884284738fdcb25318841d69ffc397b0ed0
Cole Peterson <cspeter1998@gmail.com>
2018-02-21 02:48:00 -0600

    Added Starter MVVM Project
    The project should give the boilerplate for the mvvm application.

---
+++ DiseaseSpreadModel/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiseaseSpreadModel.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string tempText;
+        public string TempText
+        {
+            get { return tempText; }
+            set { tempText = value; RaisePropertyChangedEvent("TempText"); }
+        }
+
+        public MainWindowViewModel()
+        {
+            TempText = "THIS IS BOUND TO VIEW MODEL";
+        }
+    }
+}
---
+++ DiseaseSpreadModel/ViewModels/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiseaseSpreadModel.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
