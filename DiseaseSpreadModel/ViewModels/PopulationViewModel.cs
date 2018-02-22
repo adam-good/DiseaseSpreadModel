038458f3e4d729b096e9c329afce8c903236cfb4
Cole Peterson <cspeter1998@gmail.com>
2018-02-22 13:52:52 -0600

    Added ViewModels

---
+++ DiseaseSpreadModel/ViewModels/PopulationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiseaseSpreadModel.ViewModels
+{
+    class PopulationViewModel : ViewModelBase
+    {
+        // Collection of People
+
+        public PopulationViewModel()
+        {
+
+        }
+
+        public void InitializePopulation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePopulation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DiseaseSpreadModel/ViewModels/SimulationViewModel.cs
@@ -0,0 +1,40 @@
+﻿using DiseaseSpreadModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiseaseSpreadModel.ViewModels
+{
+    class SimulationViewModel : ViewModelBase
+    {
+        // Simulation Settings
+
+        public DelegateCommand RunCommand { get; private set; }
+        public DelegateCommand ResetCommand { get; private set; }
+        public DelegateCommand PauseCommand { get; private set; }
+
+        public SimulationViewModel()
+        {
+            RunCommand = new DelegateCommand(Run);
+            ResetCommand = new DelegateCommand(Reset);
+            PauseCommand = new DelegateCommand(Pause);
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
