e19a56a3c72fdabc813be835fd96d2cd411b4146
John Townsend <john.townsend@trojans.dsu.edu>
2018-04-10 15:30:52 -0500

    Added the NumOfRuns to code and view

--- DiseaseSpreadModel/Models/SimulationSettings.cs
+++ DiseaseSpreadModel/Models/SimulationSettings.cs
@@ -9,19 +9,22 @@ namespace DiseaseSpreadModel.Models
 {
     public class SimulationSettings
     {
+        public int NumOfRuns { get; set; }
         public int CycleSpeed { get; set; }
 
         public ObservableCollection<DaysOffKeyValuePair> DaysOffList { get; set; }
 
         //TODO: add appropriate properties, framerate, etc..
-        public SimulationSettings(int cycleSpeed, ObservableCollection<DaysOffKeyValuePair> daysOffList)
+        public SimulationSettings(int numOfRuns, int cycleSpeed, ObservableCollection<DaysOffKeyValuePair> daysOffList)
         {
+            NumOfRuns = numOfRuns;
             CycleSpeed = cycleSpeed;
             DaysOffList = daysOffList;
         }
 
         public SimulationSettings(SimulationSettings simulationSettings)
         {
+            NumOfRuns = simulationSettings.NumOfRuns;
             CycleSpeed = simulationSettings.CycleSpeed;
             DaysOffList = simulationSettings.DaysOffList;
         }
